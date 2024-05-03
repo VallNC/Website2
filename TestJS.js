c49656f91fb3ef5cb4d18c362252a2e68fd0bdec
VallNC <rvall2077@gmail.com>
2024-05-03 23:19:42 +0300

    Chilling

--- TestJS.js
+++ TestJS.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+//Testing
 
 
 //////////////////////////////
@@ -38,9 +40,11 @@ const http = require('http');
 //////////////////////////////
 ///////SERVER
 const server = http.createServer((req, res) => {
-    console.log(req);
+    //console.log(req);
+    console.log(req.url);
     res.end('Hello from the server!!!!');
 })
+
 
 server.listen(8000,'127.0.0.1',()=>{
     console.log('Listening to requests on port 8000.')
