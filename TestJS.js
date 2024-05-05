31b765aa487c59c2e542c20291812e9cb5dc19c3
VallNC <rvall2077@gmail.com>
2024-05-05 11:32:11 +0300

    Transferred replaceTemplate to a standalone module

--- TestJS.js
+++ TestJS.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./Modules/replaceTemplate');
 //Testing
 
 
@@ -40,22 +41,7 @@ const url = require('url');
 //////////////////////////////
 ///////SERVER
 
-//Formatting JSON file
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
-//
+
 const tempOverview = fs.readFileSync('../1-node-farm/starter/templates/template-overview.html', 'utf8');
 const tempCard = fs.readFileSync('../1-node-farm/starter/templates/template-card.html', 'utf8');
 const tempProduct = fs.readFileSync('../1-node-farm/starter/templates/template-product.html', 'utf8');
